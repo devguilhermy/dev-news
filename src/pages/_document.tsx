5502c2693aaa1d0ad7db2916a986d011873a4f12
Guilhermy <55157846+devguilhermy@users.noreply.github.com>
2022-01-19 10:39:46 -0300

    build: created Next document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, {
+	Head,
+	Html,
+	Main,
+	NextScript
+	} from 'next/document';
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <title>Dev News</title>
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
