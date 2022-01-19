bb0517377524d68d0cdda79ceaf840a1fa49b329
Guilhermy <55157846+devguilhermy@users.noreply.github.com>
2022-01-19 10:27:39 -0300

    build: added sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import styles from '../../styles/home.module.scss';
+
 export default function Home() {
     return (
-        <div className="">
-            <h1>Hello World!</h1>
-        </div>
+        <h1 className={styles.title}>
+            Hello&nbsp;<span>World!</span>
+        </h1>
     );
 }
