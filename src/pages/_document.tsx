681d2642ea027e95045326eea48b738b9675f9f4
Guilhermy <55157846+devguilhermy@users.noreply.github.com>
2022-01-26 10:55:20 -0300

    feat: homepage finished

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,7 +20,7 @@ export default class MyDocument extends Document {
                         crossOrigin="https://fonts.gstatic.com"
                     />
                     <link
-                        href="https://fonts.googleapis.com/css2?family=Lexend:wght@400;700;900&display=swap"
+                        href="https://fonts.googleapis.com/css2?family=Lexend:wght@400;500;600;700;800;900&display=swap"
                         rel="stylesheet"
                     />
                     <link
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
+import codingImg from '../assets/images/coding.svg';
 import Head from 'next/head';
+import Image from 'next/image';
+import styles from './home.module.scss';
 
 export default function Home() {
     return (
@@ -6,7 +9,25 @@ export default function Home() {
             <Head>
                 <title>DevNews | Home</title>
             </Head>
-            <h1>Hello World!</h1>
+            <main className={styles.homeContainer}>
+                <section className={styles.homeContent}>
+                    <span>👋 Hey, Welcome</span>
+                    <h1>
+                        News about the <span>programming</span> world
+                    </h1>
+                    <p>
+                        Get access to all publications <br />
+                        <span>for $4.90 / month</span>
+                    </p>
+                    <a href="#" className={styles.cta}>
+                        Subscribe now
+                    </a>
+                </section>
+                <Image
+                    src={codingImg}
+                    alt="Girl coding on laptop on a table with books and a cup of coffee"
+                />
+            </main>
         </>
     );
 }
